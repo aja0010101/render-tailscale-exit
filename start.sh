032abfddd5aceae0459138e3c7e216ab852e6516
just for vpn <96411499+justforvpn@users.noreply.github.com>
2022-07-01 16:44:32 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+/app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
+until /app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=render-app --advertise-exit-node; do
+  sleep 0.1
+done
+sleep infinity 
+echo "Tailscale started"
